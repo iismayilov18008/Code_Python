cfec685c269d9b73c26c6882e9774f74ea1d6444
Ismayil_Ismayilov_18008 <iismayilov180008@ada.edu.az@>
2024-11-30 10:09:26 +0400

    linux day 3 partial

---
+++ listManupilation.py
@@ -0,0 +1,51 @@
+# list=[]
+# list.append(0)
+# print(list)
+# list.append(2)
+# print(list)
+# list.append(1)
+# print(list)
+# # append add element to the end
+# list.pop()
+# list.append(1)
+# print(list)
+# # pop removes elements from the end
+# list.append("salam qaqa")
+# print(list)
+# list.remove(1)
+# print(list)
+# list.pop(1)
+# # pop(indexOfElementInList)
+# print(list)
+
+# list1=[1, 2, 5]
+# list2=[6, 7, 8]
+# list1 += list2
+# print(list1) # [1, 2, 5, 6, 7, 8]
+
+# list3=list1.copy() # creates the copy of the list independent from the list3
+# list4=list1 # list4 is dependent on list1 like hardlink
+
+# list5=[1, 11, 22, 112, 223232, 42424]
+# list5.insert(1, "salam qaqa") # adds the element between 0 and 1 st element
+# print(list5)
+
+# # array sorting
+# list11=[1, 3, 2, 5, 2, 4, 6, 8, 3, 6, 6, 67, 89, 23, 4, 3]
+# print(sorted(list11)) # list11.sotr() will not work inside of a print
+# list11.sort()
+# print(list11) # [1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 6, 8, 23, 67, 89]
+# list11.sort(reverse=True)
+# print(list11) # [89, 67, 23, 8, 6, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 1]
+# listnew=sorted(list11, reverse=True)
+# print(listnew) # [89, 67, 23, 8, 6, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 1]
+
+# # here sort works without any variable assignment
+
+
+
+
+
+
+
+
